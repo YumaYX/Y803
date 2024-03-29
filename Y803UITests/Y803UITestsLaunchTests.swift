5d1f0dfd6994168efe1faaccc33f3facfb437169
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-03-30 00:16:43 +0900

    Initial Commit

---
+++ Y803UITests/Y803UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Y803UITestsLaunchTests.swift
+//  Y803UITests
+//
+//  Created by yuma on 2024/03/30.
+//
+
+import XCTest
+
+final class Y803UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
